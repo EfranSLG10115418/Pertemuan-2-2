a1606958c200c5ec52e069d1286be1cecdb04007
Efran Sucipto Lumban Gaol <efransuciptolumbangaol@gmail.com>
2017-03-01 17:22:31 +0700

    Add files via upload

---
+++ Belajar.java
@@ -0,0 +1,7 @@
+// program Belajar
+class Belajar {
+  public static void main(String argv [] ) {
+  System.out.print    ("Belajar Java Yuks");
+  System.out.println  ("Belajar Java Yuks 1");
+  }
+ }
